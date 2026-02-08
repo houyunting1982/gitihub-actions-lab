c40c981b883448d5200b973ad0f539a9b7863f48
Yunting Hou <yhou@servicetitan.com>
2026-02-07 17:23:04 -0800

    Add label policy

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -16,7 +16,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "PR", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
 app.MapGet("/weatherforecast", () =>
